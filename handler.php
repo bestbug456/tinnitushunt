5289ca968b36612fe90bd1c465568f8da23f69e5
bestbug <bestbug.corporation@gmail.com>
2015-06-09 13:14:32 +0200

    Added function to store the address

--- handler.php
+++ handler.php
@@ -3,7 +3,7 @@ if (is_ajax()) {
   if (isset($_POST["action"]) && !empty($_POST["action"])) { //Checks if action value exists
     $action = $_POST["action"];
     switch($action) { //Switch case for value of action
-      case "insert": test_function(); break;
+      case "insert": addNewAcufene(); break;
     }
   }
 }
@@ -13,8 +13,24 @@ function is_ajax() {
   return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
 }
 
-function test_function(){
-  $return = $_POST;
+function addNewAcufene(){
+
+  $adress =  $_POST['address'];
+
+  $filename = "savedArray.txt"
+  
+  $handle = fopen($filename, "r");
+  $contents = fread($handle, filesize($filename));
+  fclose($handle);
+
+  $listAddress = unserialize($contents);
+  $listAddress[hash("sha256",$adress)]++;  
+
+  file_put_contents($filename, serialize($listAddress));
+
+
+
+  $return["status"] = "ok";
   
   //Do what you need to do with the info. The following are some examples.
   //if ($return["favorite_beverage"] == ""){
