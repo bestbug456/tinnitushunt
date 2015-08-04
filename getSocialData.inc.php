48aecdb80dd278456d76d75de2860cb929f1c18d
bestbug <bestbug.corporation@gmail.com>
2015-08-04 13:30:46 +0200

    Remove some useless stuff

--- getSocialData.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-header('Content-type: application/json');
-$return['data'] = $_GET['callback'] . '(' . "{$_SESSION["userProfile"]}" . ')';
-
-$return["json"] = json_encode($return);
-echo json_encode($return);
-?>
